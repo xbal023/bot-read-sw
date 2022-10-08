8b18c5106904bf42322e1303a45199e25877064e
bolaxd <iqbalsheila619@gmail.com>
2022-10-09 00:12:54 +0700

    Auto read SW whatsapp Full light

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+	name: 'Bolaxd', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
+	autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
+	faston: 10000 // Semakin dikit nilai nya, maka semakin cepat read SW
+}
+export default config;
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const { default: KUNTUL, useMultiFileAuthState, DisconnectReason } = (await import('@adiwajshing/baileys')).default;
 import { Boom } from '@hapi/boom';
 import p from 'pino';
+import cuy from './config.js';
 
 const j = async (u, c, q) => {
 	const { lastDisconnect, connection, qr, isNewLogin, receivedPendingNotifications } = u
 	if(connection === "open") {
 		console.clear()
+		console.log('AUTO READ TELAH AKTIF!!!');
 	} else if (connection === 'close'){
 		let reason = new Boom(lastDisconnect?.error)?.output.statusCode; 
 		if (reason === DisconnectReason.badSession) q(); 
@@ -22,10 +24,11 @@ const k = async (u, c) => {
 	let m = u.messages[0]
 	//console.log(m);
 	if (m.key.remoteJid === 'status@broadcast') {
-		if (!c.auto) return
+		if (!cuy.autoread) return
 		setTimeout(async () => {
 			await c.readMessages([m.key])
-		}, 3000);
+			console.log("Auto read SW pada User :"+ m.key.remoteJid);
+		}, cuy.faston);
 	}
 };
 const start = async () => {
@@ -33,12 +36,11 @@ const start = async () => {
 		const logger = p({ level: 'silent' })
 		const { state, saveCreds } = await useMultiFileAuthState('DB');
 		const client = KUNTUL({
-			browser: ['Auto Read Sw', 'safari', '1.0.0'],
+			browser: [cuy.name, 'safari', '1.0.0'],
 			printQRInTerminal: true,
 			logger,
 			auth: state
 		});
-		client.auto = true
 		client.ev.on('messages-upsert', async (up) => k(up, client));
 		client.ev.on('connection', async (up) => j(up, client, start));
 	} catch (e) {
