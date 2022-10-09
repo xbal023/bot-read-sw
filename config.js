0f857cccad5ca5fc0d00a8e68a7bf71ba2c93d39
bolaxd <102311268+bolaxd@users.noreply.github.com>
2022-10-09 22:21:26 +0700

    Add owner

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
 	name: 'Bolaxd', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
+	owner: '6287839067186', // Ubah Nomor ke Nomor owner
 	autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
-	faston: 10000 // Semakin dikit nilai nya, maka semakin cepat read SW
+	faston: 1000 // Semakin dikit nilai nya, maka semakin cepat read SW
 }
 export default config;
